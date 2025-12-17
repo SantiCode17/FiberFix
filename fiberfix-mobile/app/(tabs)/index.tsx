69670c3ccdf6f2b619995b2d58e2e29cfbf4011f
maria <marjuriba@alu.edu.gva.es>
2025-12-17 13:37:57 +0100

    Primera plantilla de coordenadas

--- fiberfix-mobile/app/(tabs)/index.tsx
+++ fiberfix-mobile/app/(tabs)/index.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+
 
 export default function App() {
+
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrormsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      // Pedir permisos
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrormsg('Permiso de ubicación denegado');
+        return;
+      }
+
+      // Obtener ubicación actual
+      let loc = await Location.getCurrentPositionAsync({});
+      /*
+        let loc = {º
+        coords: {
+          latitude: 40.4168,
+          longitude: -3.7038,
+        },
+      };*/
+      setLocation(loc as Location.LocationObject);
+    })();
+  }, [])
+
   return (
-    <View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>FiberFix - App Técnico</Text>
+
+      {errorMsg && <Text>{errorMsg}</Text>}
+
+      {location && (
+        <>
+          <Text>Latitud: {location.coords.latitude}</Text>
+          <Text>Longitud: {location.coords.longitude}</Text>
+        </>
+      )}
+
+      {!location && !errorMsg && (
+        <Text>Obteniendo ubicación...</Text>
+      )}
     </View>
   )
 }
