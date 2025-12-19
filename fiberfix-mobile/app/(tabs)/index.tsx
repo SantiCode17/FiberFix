3359c8d5101b044a308e05cdf688f50ecf94fbb7
maria <marjuriba@alu.edu.gva.es>
2025-12-19 12:31:54 +0100

    Location separada en component y hook

--- fiberfix-mobile/app/(tabs)/index.tsx
+++ fiberfix-mobile/app/(tabs)/index.tsx
@@ -1,51 +1,20 @@
+import { LocationInfo } from '@/components/LocationInfo';
+import { useLocation } from '@/hooks/useLocation';
 import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 
-
 export default function App() {
 
-  const [location, setLocation] = useState<Location.LocationObject | null>(null);
-  const [errorMsg, setErrormsg] = useState<string | null>(null);
+  // EVIDENCIA 2: GEOLOCALIZACIÓN
 
-  useEffect(() => {
-    (async () => {
-      // Pedir permisos
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrormsg('Permiso de ubicación denegado');
-        return;
-      }
-
-      // Obtener ubicación actual
-      let loc = await Location.getCurrentPositionAsync({});
-      /*
-        let loc = {º
-        coords: {
-          latitude: 40.4168,
-          longitude: -3.7038,
-        },
-      };*/
-      setLocation(loc as Location.LocationObject);
-    })();
-  }, [])
-
+  const { location, errorMsg, loading } = useLocation();
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>FiberFix - App Técnico</Text>
-
-      {errorMsg && <Text>{errorMsg}</Text>}
-
-      {location && (
-        <>
-          <Text>Latitud: {location.coords.latitude}</Text>
-          <Text>Longitud: {location.coords.longitude}</Text>
-        </>
-      )}
-
-      {!location && !errorMsg && (
-        <Text>Obteniendo ubicación...</Text>
-      )}
+        {errorMsg && <Text>{errorMsg}</Text>}
+        {loading && <Text>Obteniendo ubicación...</Text>}
+        {location && <LocationInfo location={location}/>}
     </View>
   )
 }
